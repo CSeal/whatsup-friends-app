cb3c40f726c31df95dd01aab2a4cb8e36a7a3e83
anton <19mann84@gmail.com>
2023-04-18 00:36:40 +0300

    setup react-native-testing-library and fixed the  problem with jest for third-party libraries like react-native-ui-lib

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    '@babel/plugin-proposal-private-methods',
+    '@babel/plugin-proposal-class-properties',
     [
       'react-native-reanimated/plugin',
       {
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import {Navigation} from 'react-native-navigation';
 import {name as appName} from './app.json';
 import App from './App';
